a2811fd879a9402aec179ecfadb80f073193d0c5
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2022-09-07 19:51:31 -0700

    github logo

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,4 +2,12 @@ import { defineConfig } from "vitepress";
 
 export default defineConfig({
   title: "TypeScript Monorepo Cheatsheet",
+  themeConfig: {
+    socialLinks: [
+      {
+        icon: "github",
+        link: "https://github.com/lawrencecchen/monorepo-cheatsheet",
+      },
+    ],
+  },
 });
